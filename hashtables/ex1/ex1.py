ddf3e61e30ca46647791a0bb51ce5c98e6b148bd
Natalie McCroy <nataliemccroy@gmail.com>
2020-04-23 15:46:55 -0500

    ex1 tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,25 +1,25 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
-
+from hashtables import HashTable, hash_table_insert, hash_table_retrieve
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-    print(ht)
-    """
-    YOUR CODE HERE
-    """
-    for i in weights:
-        ht.hash_table_insert(weights[i], weights.index(i) )
 
+    for i in range(0, len(weights)):
+        n = hash_table_retrieve(ht, limit - weights[i])
+        
+           
+        if n is not None:
+            return (i, n)
+        else:
+            hash_table_insert(ht, weights[i], i)
+            # answer = (n, i)
+            # return answer
+                # [my_ht[n], i]
     return None
-
 
 def print_answer(answer):
     if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
